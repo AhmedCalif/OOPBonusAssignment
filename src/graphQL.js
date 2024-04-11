60301f6b6d03bba575b0a89192d6ec7deeee98dd
ahmedcalif88 <ahmedcalif88@gmail.com>
2024-04-11 09:52:02 -0700

    finish tag feature

--- src/graphQL.js
+++ src/graphQL.js
@@ -3,70 +3,106 @@
 import { randomUUID } from "crypto";
 
 export const schema = `
+  type Tag {
+    id: ID!
+    name: String!
+  }
+
   type Post {
     id: ID!
     title: String!
     content: String!
+    tag: Tag!
   }
 
   input PostCreate {
     title: String!
     content: String!
+    tagId: ID!
   }
 
   type Query {
     getPosts: [Post!]!
     getPost(id: ID!): Post
+    getTags: [Tag!]!
+    getPostsByTag(tagId: ID!): [Post!]!
+    getTag(id: ID!): Tag
   }
 
   type Mutation {
     createPost(newPost: PostCreate!): Post!
     deletePost(id: ID!): Post!
-    updatePost(id: ID!, newPost: PostCreate!): Post!
+    updatePost(id: ID!, updatedPost: PostCreate!): Post!
+    createTag(name: String!): Tag!
   }
 `;
 
 export const resolvers = {
   Query: {
-    getPosts: (_parent, args, { app }) => {
-      return app.db.posts;
-    },
-    getPost: (_parent, args, { app }) => {
-      const { id } = args;
-      return app.db.posts.find((post) => post.id === id);
-    },
+    getPosts: (_parent, _args, { app }) => app.db.posts,
+    getPost: (_parent, { id }, { app }) => app.db.posts.find((post) => post.id === id),
+    getTags: (_parent, _args, { app }) => app.db.tags,
+    getPostsByTag: (_parent, { tagId }, { app }) => app.db.posts.filter((post) => post.tag.id === tagId),
+    getTag: (_parent, { id }, { app }) => app.db.tags.find((tag) => tag.id === id),
   },
   Mutation: {
     createPost: (_parent, { newPost }, { app }) => {
-      const { title, content } = newPost;
-
+      const { title, content, tagId } = newPost;
+      app.db.tags = app.db.tags || [];
+    
+      const tag = app.db.tags.find((tag) => tag.id === tagId);
+    
+      if (!tag) {
+        throw new Error('Tag not found');
+      }
+    
       const post = {
         id: randomUUID(),
         title,
         content,
+        tag
       };
+      app.db.posts = app.db.posts || [];
       app.db.posts.push(post);
+    
       return post;
     },
     deletePost: (_parent, { id }, { app }) => {
       const postIndex = app.db.posts.findIndex((post) => post.id === id);
       if (postIndex === -1) {
-        return null;
+        throw new Error('Post not found');
       }
       return app.db.posts.splice(postIndex, 1)[0];
     },
-    updatePost: (_parent, { id, newPost }, { app }) => {
-      const { title, content } = newPost;
+    updatePost: (_parent, { id, updatedPost }, { app }) => {
+      const { title, content, tagId } = updatedPost;
       const postIndex = app.db.posts.findIndex((post) => post.id === id);
+      const tag = app.db.tags.find((tag) => tag.id === tagId);
+
       if (postIndex === -1) {
-        return null;
+        throw new Error('Post not found');
       }
+      if (!tag) {
+        throw new Error('Tag not found');
+      }
+
       const post = app.db.posts[postIndex];
       post.title = title;
       post.content = content;
+      post.tag = tag;
       return post;
     },
-  },
+    createTag: (_parent, { name }, { app }) => {
+      app.db.tags = app.db.tags || [];
+      
+      const tag = {
+        id: randomUUID(),
+        name
+      };
+      app.db.tags.push(tag);
+      return tag;
+    }
+  }
 };
 
 export const loaders = {};
--- src/plugins/db/index.js
+++ src/plugins/db/index.js
@@ -8,28 +8,48 @@ export const posts = [
     id: "a0a0a0a0-a0a0-a0a0-a0a0-a0a0a0a0a0a0",
     title: "Why I love GraphQL",
     content: "End to End Typesafety is great.",
+    tag: {
+      id: "t0t0t0t0-t0t0-t0t0-t0t0-t0t0t0t0t0t0",
+      name: "GraphQL",
+    },
   },
   {
     id: "b1b1b1b1-b1b1-b1b1-b1b1-b1b1b1b1b1b1",
     title: "How to write a song",
     content: "Study music theory or have a natural ear for it.",
+    tag: {
+      id: "t1t1t1t1-t1t1-t1t1-t1t1-t1t1t1t1t1t1",
+      name: "Music",
+    },
   },
   {
     id: "c2c2c2c2-c2c2-c2c2-c2c2-c2c2c2c2c2c2",
     title: "How to travel the world",
     content: "Start planning early and make sure you save up enough money",
+    tag: {
+      id: "t2t2t2t2-t2t2-t2t2-t2t2-t2t2t2t2t2t2",
+      name: "Travel",
+    },
   },
   {
     id: "d3d3d3d3-d3d3-d3d3-d3d3-d3d3d3d3d3d3",
     title: "How to travel to Asia",
     content:
       "Do research on which area in Asia you want to visit (ex - India, China, etc), and learn a bit of the local language, perhaps with Duolingo",
+    tag: {
+      id: "t3t3t3t3-t3t3-t3t3-t3t3-t3t3t3t3t3t3",
+      name: "Travel",
+    },
   },
   {
     id: "e4e4e4e4-e4e4-e4e4-e4e4-e4e4e4e4e4e4",
     title: "How to travel to Iceland",
     content:
       "Traveling to Iceland is really cool, be sure to bring a good jacket or rent one while visiting.",
+    tag: {
+      id: "t4t4t4t4-t4t4-t4t4-t4t4-t4t4t4t4t4t4",
+      name: "Travel",
+    },
   },
 ];
 
